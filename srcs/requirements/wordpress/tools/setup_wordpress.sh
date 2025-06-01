76abc7e23ed01dd772d568534448d6019fc920e5
said <said@gmail.com>
2025-06-01 08:47:11 +0100

    addd

---
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://wordpress.org/wordpress-6.8.1.tar.gz
+tar -xvzf wordpress-6.8.1.tar.gz
+mv wordpress/* /wordpress/
+mv wp-config.php /wordpress/
+mv www.conf $(find /etc/php/ -type f -name www.conf)
+rm -rf wordpress wordpress-6.8.1.tar.gz
+mkdir -p /run/php
+$(find /usr/sbin/ | grep php-fpm) -F
+
