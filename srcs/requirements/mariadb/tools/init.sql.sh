ce7c2fba9bf7d631ad9b9e7c0ffa6a0327e246d0
said <said@gmail.com>
2025-06-25 12:17:00 +0100

    addallservices

--- srcs/requirements/mariadb/tools/init.sql.sh
+++ srcs/requirements/mariadb/tools/init.sql.sh
@@ -13,13 +13,13 @@ msgerror=$(mysqladmin ping -u $MYSQL_NAME_ROOT -p"$MYSQL_PASS_ROOT" 2>&1  1> /de
 
 if [ "$msgerror" != "" ]; then
 echo " ❗️ start create users and database in mariadb"
-<<EOF cat | mariadb && echo " ✅ create users and database in mariadb successfully ✅" || echo " ❌️ create users and database in mariadb failed ❌️" 
+<<EOF cat | mariadb && echo " ✅ create users and database in mariadb successfully ✅" || echo " ❌️ create users and database in mariadb failed ❌️"
 ALTER USER 'root'@'localhost' IDENTIFIED BY "$MYSQL_PASS_ROOT";
 RENAME USER 'root'@'localhost' TO "$MYSQL_NAME_ROOT"@'localhost';
 CREATE DATABASE $MYSQL_WORDPRESS;
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS';
 GRANT ALL PRIVILEGES ON $MYSQL_WORDPRESS.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES; 
+FLUSH PRIVILEGES;
 EOF
 
 else
--- srcs/requirements/nginx/tools/start_nginx.sh
+++ srcs/requirements/nginx/tools/start_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until nc -vz wordpress_php 9000 ; do
+until nc -vz wordpress_php 9000 2> /dev/null; do
     echo "⏳ nginx Waiting for wordpress_php"
     sleep 1
 done
